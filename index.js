6c77520bab9207d0cf582f61c0554e89613e4c20
SpeziFisch-DE <67187041+SpeziFisch-DE@users.noreply.github.com>
2021-03-11 18:26:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,31 @@ const client = new Discord.Client();
 const Http = require("http");
 const Url = require("url");
 const Mongo = require("mongodb");
-const { Interface } = require('node:readline');
-const { randomInt } = require('node:crypto');
 
+client.on("ready", () => {
+  console.log("I am ready!");
+});
 
+client.on("message", message => {
+  if (message.author.bot) return;
+  // The process.env.PREFIX is your bot's prefix in this case.
+  if (message.content.indexOf(process.env.PREFIX) !== 0) return;
+
+  // This is the usual argument parsing we love to use.
+  const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  // And our 2 real basic commands!
+  if(command === 'ping') {
+    message.channel.send('Pong!');
+  } else
+  if (command === 'blah') {
+    message.channel.send('Meh.');
+  }
+});
 
 
 let users;
-let words;
 let port = Number(process.env.PORT);
 if (!port)
     port = 8100;
@@ -28,8 +45,7 @@ async function connectToDatabase(_url) {
     let options = { useNewUrlParser: true, useUnifiedTopology: true };
     let mongoClient = new Mongo.MongoClient(_url, options);
     await mongoClient.connect();
-    users = mongoClient.db("Test").collection("hangman");
-    words = mongoClient.db("Test").collection("words");
+    users = mongoClient.db("Test").collection("users");
     console.log("Database connected: " + users != undefined);
 }
 function handleListen() {
@@ -38,34 +54,6 @@ function handleListen() {
 function handleRequest() {
   console.log("request send");
 }
-
-
-let word;
-
-client.on("ready", () => {
-  console.log("I am ready!");
-});
-
-client.on("message", message => {
-  if (message.author.bot) return;
-  // The process.env.PREFIX is your bot's prefix in this case.
-  if (message.content.indexOf(process.env.PREFIX) !== 0) return;
-
-  // This is the usual argument parsing we love to use.
-  const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  const param = args.shift().toLowerCase();
-
-  // And our 2 real basic commands!
-  if(command === 'ping') {
-    message.channel.send('Pong!');
-  } else
-  if (command === 'hangman'){
-    let wordlist = words.findOne({ "words": "words"});
-    word = wordlist.word[randomInt(wordlinst.word.length)];
-    message.channel.send(word);
-  }
-});
 
 // There's zero need to put something here. Discord.js uses process.env.CLIENT_TOKEN if it's available,
 // and this is what is being used here. If on discord.js v12, it's DISCORD_TOKEN
