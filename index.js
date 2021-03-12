e6cdb9bf8e9cee2a24a6da1c4428386756c6e0a6
SpeziFisch-DE <67187041+SpeziFisch-DE@users.noreply.github.com>
2021-03-12 10:25:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ client.on("message", message => {
     message.channel.send('Pong!');
   } else
   if (command === 'link') {
-    message.channel.send("wake me up [here](spezifisch-de.github.io/MyHangmanBot/index.html)");
+    let myEmbed;
+    myEmbed.addField("","wake me up [here](spezifisch-de.github.io/MyHangmanBot/index.html)")
+    message.channel.send(myEmbed);
   } else
   if (command === 'hangman'){
     async function loadWordList() {
