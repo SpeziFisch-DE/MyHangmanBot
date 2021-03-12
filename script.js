ac5ec67bc27dbfea258598cb71c77426989c20cc
SpeziFisch-DE <67187041+SpeziFisch-DE@users.noreply.github.com>
2021-03-12 09:45:43 +0100

    test

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 "use strict";
+let url = "http://spezifisch-hangman.herokuapp.com/?test";
+async function wakeServer() {
+    await fetch(url);
+}
+window.open("index.html?test", "_self");
 window.close();
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,1 +1,6 @@
+let url: string = "http://spezifisch-hangman.herokuapp.com/?test";
+async function wakeServer(): Promise<void> {
+    await fetch(url);
+}
+window.open("index.html?test", "_self");
 window.close();
