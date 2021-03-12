b648d64345531bfeceb70f0286f5d04b332d5b1d
SpeziFisch-DE <67187041+SpeziFisch-DE@users.noreply.github.com>
2021-03-12 09:48:56 +0100

    test

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 "use strict";
+function getSubpage() {
+    return window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1);
+}
 let url = "http://spezifisch-hangman.herokuapp.com/?test";
 async function wakeServer() {
     await fetch(url);
 }
 window.open("index.html?test", "_self");
-window.close();
+if (getSubpage() == "index.html?test") {
+    window.close();
+}
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,6 +1,12 @@
+function getSubpage(): string {
+    return window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1);
+}
+
 let url: string = "http://spezifisch-hangman.herokuapp.com/?test";
 async function wakeServer(): Promise<void> {
     await fetch(url);
 }
 window.open("index.html?test", "_self");
-window.close();
+if (getSubpage() == "index.html?test") {
+    window.close();
+}
